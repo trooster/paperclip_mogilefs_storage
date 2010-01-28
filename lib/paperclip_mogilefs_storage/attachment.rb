f10af465b7c3915f1dabc71777b17cc7ecfb048e
trooster <trooster@interstroom.nl>
2010-01-28 16:50:01 +0100

    find mogilefs storage paths: user.photo.mogilefs_paths(type)

--- lib/paperclip_mogilefs_storage/attachment.rb
+++ lib/paperclip_mogilefs_storage/attachment.rb
@@ -1,10 +1,20 @@
 module Paperclip
   class Attachment
-    
+
     def mogilefs_url style = default_style
       mogilefs = MogileFSConnect.connect_tracker
       url = original_filename.nil? ? interpolate(@default_url, style) : interpolate(@url, style)
       mogilefs.get_paths(url)[0]
+      rescue MogileFS::Backend::UnknownKeyError
+        Paperclip.logger.error("[paperclip] Error: #{url} not found in MogileFS")
+    end
+
+    def mogilefs_get_paths style = default_style
+      mogilefs = MogileFSConnect.connect_tracker
+      url = original_filename.nil? ? interpolate(@default_url, style) : interpolate(@url, style)
+      mogilefs.get_paths(url)
+      rescue MogileFS::Backend::UnknownKeyError
+        Paperclip.logger.error("[paperclip] Error: #{url} not found in MogileFS")
     end
   
   end
